21dfac4ba66b00838fd4a67afcb6081450c4e935
Sarah Morris <sarah.morris@geappliances.com>
2026-01-05 15:35:15 -0500

    First commit

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdIn;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String input = StdIn.readString();
+        System.out.println("Hello, World" + input);
+    }
+}
---
+++ RandomWorld.java
@@ -0,0 +1,26 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWorld {
+    public static void main(String[] args) {
+        int count = 0;
+        String champion = "";
+        while (!StdIn.isEmpty()) {
+            String challenger = StdIn.readString();
+            count++;
+
+            if (StdRandom.bernoulli(1.0 / count)) {
+                champion = challenger;
+            }
+        }
+        StdOut.println(champion);
+
+    }
+}
